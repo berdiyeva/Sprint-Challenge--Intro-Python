a3e87a89b1ccb940dc6a36b0d26d0188eb1de602
berdiyeva <berdiyevaaltyn@gmail.com>
2020-08-15 12:14:22 -0500

    mvp code is here

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,14 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __repr__(self):
+    return(f"City: {self.name}, Latitude: {self.lat}, Longtitude: {self.lon}")
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,15 +22,21 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
-cities = []
 
+cities = []
+import csv
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+
+  with open("src/cityreader/cities.csv", newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      city = City(row["city"], float(row["lat"]), float(row["lng"]))
+      cities.append(city)
+  return cities
 
 cityreader(cities)
 
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# BASE CLASS
+class Vehicle:
+    # def __init__(self):
+        pass
+
+# FlightVehicle hierarchy
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass 
+
+# GroundVehicle hierarchy
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
